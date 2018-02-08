a1cdc05a0d4e802304d248c8ab65fa2e446010c3
Zhangyi Chen <frozen.zju@gmail.com>
2018-02-08 17:16:41 +0800

    Add travis related stuff

---
+++ build_in_travis_ci.sh
@@ -0,0 +1,39 @@
+if [ -z "$PURPOSE" ]; then
+    echo "PURPOSE must be set"
+    exit 1
+fi
+if [ -z "$CXX" ]; then
+    echo "CXX must be set"
+    exit 1
+fi
+if [ -z "$CC" ]; then
+    echo "CC must be set"
+    exit 1
+fi
+
+runcmd(){
+    eval $@
+    [[ $? != 0 ]] && {
+        exit 1
+    }
+    return 0
+}
+
+echo "build combination: PURPOSE=$PURPOSE CXX=$CXX CC=$CC"
+
+rm -rf build && mkdir build && cd build
+if [ "$PURPOSE" = "compile" ]; then
+    if ! cmake ..; then
+        echo "Fail to generate Makefile by cmake"
+        exit 1
+    fi
+    make -j4
+elif [ "$PURPOSE" = "unittest" ]; then
+    if ! cmake -DBUILD_UNIT_TESTS=ON ..; then
+        echo "Fail to generate Makefile by cmake"
+        exit 1
+    fi
+    make -j4 && cd test && sh ../../test/run_tests.sh && cd ../
+else
+    echo "Unknown purpose=\"$PURPOSE\""
+fi
